3a28a4b9b5b5ddab6376447c2d27ec70a0e6b5e4
IdentityZero <lovelycadiente1@gmail.com>
2024-03-30 17:00:48 +0800

    Added cors allowed origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,6 +86,9 @@ TEMPLATES = [
 WSGI_APPLICATION = "backend.wsgi.application"
 ASGI_APPLICATION = "backend.asgi.application"
 EVENTSTREAM_ALLOW_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = [
+    '*',
+]
 
 # EVENTSTREAM_ALLOW_ORIGINS = ['http://192.168.100.144:8002',
 #     'http://192.168.100.144:8000',]
@@ -159,5 +162,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
-
-CORS_ALLOW_ALL_ORIGINS = True
