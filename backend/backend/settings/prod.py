c3ed5ddd3cf9bdfa3993cbfef972dea5e51fc97c
IdentityZero <lovelycadiente1@gmail.com>
2024-05-22 18:18:53 +0800

    Debugging

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -13,7 +13,7 @@ ALLOWED_HOSTS = ['47.129.54.22', 'localhost','127.0.0.1', 'coemobility.com']
 
 EVENTSTREAM_ALLOW_ORIGINS = ['*']
 CORS_ALLOWED_ORIGINS = [
-    'http://47.129.54.22',
+    'http://47.129.54.22:8000',
     'https://47.129.54.22',
     'http://localhost',
     'http://127.0.0.1',
